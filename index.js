a7b0591195c73c639f8a87e195c2a5bd512399c6
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-04-25 00:29:26 +0000

    private rpc

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+const proxyMiddleware = createProxyMiddleware({
+  target: process.env.FIGMENT_URL,
+  changeOrigin: true,
+  headers: {
+    Authorization: process.env.FIGMENT_TOKEN,
+  }
+});
+
+const app = express()
+app.use('/', proxyMiddleware);
+app.listen(process.env.PORT || 8080);
+
